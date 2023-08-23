bb8e116ec353cb5a287746ce639b758427cde6e2
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2023-08-23 10:46:43 +0530

    navigition link commit

---
+++ src/pages/Error.js
@@ -0,0 +1,17 @@
+import MainNavigation from "../components/MainNavigation";
+
+const ErrorPage = () => {
+    return (
+        <>
+            <MainNavigation></MainNavigation>
+            <main>
+                <h1>
+                    There is an error..
+                </h1>
+            </main>
+
+        </>
+    )
+}
+
+export default ErrorPage;
